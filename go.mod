069c1557945c9bd2e2a19d1adbc143066d1afe26
JavaHutt <alxmer111@gmail.com>
2021-08-25 23:12:05 +0400

    added docx library

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JavaHutt/contract-template-generator
+
+go 1.16
+
+require github.com/nguyenthenguyen/docx v0.0.0-20210714134305-d2271ea4db67
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/nguyenthenguyen/docx"
+)
 
 func main() {
-	fmt.Println(1)
+	r, err := docx.ReadDocxFile("./template.docx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+	docx1 := r.Editable()
+	// Replace like https://golang.org/pkg/strings/#Replace
+	if err := docx1.Replace("общая сумма контракта", "8 000 000 (восемь миллионов) Долларов США", -1); err != nil {
+		log.Fatal(err)
+	}
+	docx1.WriteToFile("./result.docx")
 }
